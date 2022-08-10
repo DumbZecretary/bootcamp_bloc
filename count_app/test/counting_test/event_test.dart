695a19be22a13cdb6cf01c453090b208b64325b9
DumbZecretary <sayuelfreda@gmail.com>
2022-08-10 20:52:57 +0700

    add todo for Count App

--- count_app/test/counting_test/event_test.dart
+++ count_app/test/counting_test/event_test.dart
@@ -118,13 +118,17 @@ void main() {
   });
 }
 
+// TODO: make make file
+// TODO: create read me make down to note the knowledge
 // TODO: integration testing
-// TODO: make snippet for built value
 // TODO: repository share reference
+// TODO: dependency injection
+// TODO: make snippet for built value
 // TODO: repository sql lite
 // TODO: repository hive
 // TODO: App Bar with the title
 // TODO: localization
 // TODO: splash screen
+// TODO: notification - permission
 
 // to prevent seeing error on android folder for flutter project, open a new flutter project without error then reopen this project
